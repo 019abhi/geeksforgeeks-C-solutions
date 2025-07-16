b571e816816b4291f27d6de083598fb1fa5ac2ab
Your Name <abhishekgobindgarhia019@gmail.com>
2025-07-16 22:30:04 +0530

    success

--- Math/NineDivisors.cpp
+++ Math/NineDivisors.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int countNumbers(int n) {
         // Code Here
         int result = 0;
-         for (int i = 0; i*i <= n; i++){
+         for (int i = 1; i*i <= n; i++){
              int count = 0;
              int sq = i*i;
 
-             for (int j=1; j*j<sq ; j++){
+             for (int j=1; j*j<=sq ; j++){
                  if(sq % j)continue;
                  count++;
                  if(sq/j != j) count++;
@@ -21,7 +21,7 @@ int countNumbers(int n) {
              result++;
          }
          return result;
-    };
+    }
 
     int main(){
         int n;
